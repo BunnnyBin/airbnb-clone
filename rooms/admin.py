59b1faff8edd021344045d1a3e9bcfed774d97b1
BunnyBin <dbqls8428@gmail.com>
2020-04-03 00:29:58 +0900

    #8.8 Intercepting Model save() and admin_save()

--- rooms/admin.py
+++ rooms/admin.py
@@ -25,7 +25,7 @@ class RoomAdmin(admin.ModelAdmin):
     fieldsets = (
         (
             "Basic Info",
-            {"fields": ("name", "description", "country", "address", "price")}
+            {"fields": ("name", "description", "country", "city", "address", "price")}
         ),
         (
             "Times",
@@ -85,6 +85,10 @@ class RoomAdmin(admin.ModelAdmin):
         "facilities",
         "house_rules",
     )
+
+    def save_model(self, request, obj, form, change):
+        print(obj, change, form)
+        super().save_model(request, obj, form, change)
 
     def count_amenities(self, obj):
         return obj.amenities.count()
