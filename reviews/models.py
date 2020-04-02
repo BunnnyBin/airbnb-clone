bb9dff3299a8fdb866563a834050c09c53eff85f
BunnyBin <dbqls8428@gmail.com>
2020-04-02 17:22:45 +0900

    #8.0 Rewiew Admin and Room Average

--- reviews/models.py
+++ reviews/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from core import models as core_models
 
-class Review(core_models.TimeStampedModel):
 
+class Review(core_models.TimeStampedModel):
     review = models.TextField()
     accuracy = models.IntegerField()
     communication = models.IntegerField()
@@ -15,3 +15,14 @@ class Review(core_models.TimeStampedModel):
 
     def __str__(self):
         return f'{self.review} - {self.room}'
+
+    def rating_average(self):
+        avg = (self.accuracy
+               + self.communication
+               + self.cleanliness
+               + self.location
+               + self.check_in
+               + self.value) / 6
+        return round(avg, 2)
+
+    rating_average.short_description = "AVG."
--- users/admin.py
+++ users/admin.py
@@ -13,7 +13,30 @@ class CustomUserAdmin(UserAdmin):
         (
             "Custom Profile",
             {
-                "fields": ("avatar", "gender", "bio","birthdate","language","currency","superhost")
+                "fields": ("avatar",
+                           "gender",
+                           "bio",
+                           "birthdate",
+                           "language",
+                           "currency",
+                           "superhost")
             }
         ),
+    )
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "language",
+        "currency",
+        "superhost",
+        "is_staff",
+        "is_superuser",
+    )
+
+    list_filter = UserAdmin.list_filter + (
+        "superhost",
     )
