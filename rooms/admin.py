c17346546abc2d7270d926b9daafef3e11426040
BunnyBin <dbqls8428@gmail.com>
2020-04-03 00:08:43 +0900

    #8.6 raw_ids and Inline Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
+
 
 @admin.register(models.RoomType, models.HouseRule, models.Facility, models.Amenity)
 class ItemAdmin(admin.ModelAdmin):
@@ -11,16 +13,23 @@ class ItemAdmin(admin.ModelAdmin):
     def used_by(self, obj):
         return obj.rooms.count()
 
+
+class PhotoInline(admin.TabularInline):
+    model = models.Photo
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+    inlines = (PhotoInline,)
+
     fieldsets = (
         (
-        "Basic Info",
-        {"fields": ("name","description","country","address","price")}
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")}
         ),
         (
             "Times",
-            {"fields": ("check_in","check_out","instant_book")}
+            {"fields": ("check_in", "check_out", "instant_book")}
         ),
         (
             "Space",
@@ -28,12 +37,12 @@ class RoomAdmin(admin.ModelAdmin):
         ),
         (
             "More About the Space",
-            {"classes" : ('collapse',),
-             "fields":("amenities","facilities","house_rules")}
+            {"classes": ('collapse',),
+             "fields": ("amenities", "facilities", "house_rules")}
         ),
         (
             "Last Details",
-            {"fields":("host",)}
+            {"fields": ("host",)}
         ),
     )
 
@@ -67,6 +76,8 @@ class RoomAdmin(admin.ModelAdmin):
         "country",
     )
 
+    raw_id_fields = ("host",)
+
     search_fields = ("city", "host__username")
 
     filter_horizontal = (
@@ -84,4 +95,9 @@ class RoomAdmin(admin.ModelAdmin):
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("__str__", "get_thumbnail")
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img width="50px" src="{obj.file.url}" />')
+
+    get_thumbnail.short_description = "Thumbnail"
