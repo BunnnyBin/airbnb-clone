d4ebece4952350e1c2b0867e831e78c081108c1e
BunnyBin <dbqls8428@gmail.com>
2020-04-17 14:08:52 +0900

    #13.4 Finishing the Form

--- rooms/views.py
+++ rooms/views.py
@@ -59,8 +59,10 @@ def search(request):
     bedrooms = int(request.GET.get("bedrooms", 0))
     beds = int(request.GET.get("beds", 0))
     baths = int(request.GET.get("baths", 0))
-    s_amenities = request.GET.get("amenities")
-    s_facilities = request.GET.get("facilities")
+    instant = request.GET.get("instant", 0)
+    super_host = request.GET.get("super_host", 0)
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
 
     # form에서 받은 값
     form = {
@@ -74,6 +76,8 @@ def search(request):
         "baths": baths,
         "s_amenities":s_amenities,
         "s_facilities":s_facilities,
+        "instant" : instant,
+        "super_host" : super_host,
     }
 
     room_types = models.RoomType.objects.all()
