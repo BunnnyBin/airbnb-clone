2c5a78c8782b5459c1e5a29c2680af95b540397c
BunnyBin <dbqls8428@gmail.com>
2020-04-26 00:40:30 +0900

    #14.0 Setting Up!

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class LoginView(View):
+    def get(self, request):  # function : if request.method == "GET"
+        return render(request, "users/login.html")
+
+    def post(self, request):
+        pass
