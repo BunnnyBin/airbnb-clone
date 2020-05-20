e7283a6f54f83ae90cec7f0ce4afc8cff1b75e0a
BunnyBin <dbqls8428@gmail.com>
2020-05-21 00:15:26 +0900

    #20.1 Header part One

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,11 @@ const css = () => {
   const minify = require("gulp-csso");
   sass.compiler = require("node-sass");
   return gulp
-    .src("assets/scss/styles.scss")
+    .src("assets/scss/styles.scss")  //sass파일을 찾는다
     .pipe(sass().on("error", sass.logError))
-    .pipe(postCSS([require("tailwindcss"), require("autoprefixer")]))
-    .pipe(minify())
-    .pipe(gulp.dest("static/css"));
+    .pipe(postCSS([require("tailwindcss"), require("autoprefixer")])) //sass파일을 css로 만든다.
+    .pipe(minify()) //간편화
+    .pipe(gulp.dest("static/css"));  //css파일에 저장한다. - 이 과정을 npm run css
 };
 
 exports.default = css;
