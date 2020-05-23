d28d718a0cccfff12e14d0633b8a61c56dea663c
BunnyBin <dbqls8428@gmail.com>
2020-05-23 18:22:57 +0900

    #21.6 Update Profile

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ import os, requests
 from django.shortcuts import render, redirect, reverse
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import FormView, DetailView
+from django.views.generic import FormView, DetailView, UpdateView
 from django.contrib.auth import authenticate, login, logout
 from django.core.files.base import ContentFile
 from . import forms, models
@@ -215,3 +215,20 @@ class UserProfileView(DetailView):
     #     context = super().get_context_data(**kwargs)
     #     context["hello"] = "Gggg"
     #     return context
+
+class UpdateProfileView(UpdateView):
+    model = models.User
+    template_name = "users/update-profile.html"
+    fields = (
+        "first_name",
+        "last_name",
+        "avatar",
+        "gender",
+        "bio",
+        "birthdate",
+        "language",
+        "currency",
+    )
+    # url로 pk를 넘기지 않으므로 필요한 객체를 가져와야 한다.
+    def get_object(self, queryset=None):
+        return self.request.user
