99f9df78d8c4eba48b0cb2f79f2a50603669a199
BunnyBin <dbqls8428@gmail.com>
2020-07-05 11:11:35 +0900

    #25.5 Fav Template Tag
    make template tags

--- lists/urls.py
+++ lists/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "lists"
 
 urlpatterns = [
-    path("add/<int:room_pk>/", views.save_room, name="save_room"),
+    path("add/<int:room_pk>/", views.save_room, name="save-room"),
 
 ]
--- lists/views.py
+++ lists/views.py
@@ -5,7 +5,7 @@ from . import models
 def save_room(request, room_pk):
     room = room_models.Room.objects.get_or_none(pk=room_pk)
     if room is not None:
-        #get은 하나의 Object만 찾아서 두개 이상이면 에러가 난다.
+        #get은 하나의 Object만 찾아서 두개 이상이면 에러가 난다. -> OneToOneField models
         #created - for unpack tuple
         the_list, created = models.List.objects.get_or_create(user=request.user, name="My Favorite Houses")
         the_list.rooms.add(room)
