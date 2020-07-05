4ed0c3ac4d0f7636402ea2a1fa6ce14941673e55
BunnyBin <dbqls8428@gmail.com>
2020-07-05 10:55:32 +0900

    #25.4 Saving Fav Rooms
    lists

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from rooms import models as room_models
+from . import models
 
-# Create your views here.
+def save_room(request, room_pk):
+    room = room_models.Room.objects.get_or_none(pk=room_pk)
+    if room is not None:
+        #get은 하나의 Object만 찾아서 두개 이상이면 에러가 난다.
+        #created - for unpack tuple
+        the_list, created = models.List.objects.get_or_create(user=request.user, name="My Favorite Houses")
+        the_list.rooms.add(room)
+    return redirect(reverse("rooms:detail", kwargs={"pk":room_pk}))
